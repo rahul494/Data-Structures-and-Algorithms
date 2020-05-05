d86fdd80d7d87e5e2a4997f84cd879e7a4b2642c
rahul494 <rahul494@my.yorku.ca>
2020-05-04 21:34:16 -0400

    Commit

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,14 +1,11 @@
 def insertionSort(arr):
     for i in range(len(arr)):
-        for j in range(len(arr[:-1])):
+        for j in range(len(arr)-i-1):
             if arr[j] > arr[j+1]:
                 temp = arr[j]
                 arr[j] = arr[j+1]
-                arr[j+1] = temp 
-
-                print(arr)
-    
-
-
+                arr[j+1] = temp
+  
+    print(arr)
 
 insertionSort([4, 3, 2, 10, 12, 1, 5, 6])
