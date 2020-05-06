e25cd2fae7b1b9f5e91298f492e5f3a7faac96bb
rahul494 <rahul494@my.yorku.ca>
2020-05-06 17:07:34 -0400

    Added more test cases

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,11 +1,16 @@
-def insertionSort(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr)-i-1):
-            if arr[j] > arr[j+1]:
-                temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = temp
-  
-    print(arr)
+def bubbleSort(array):
+    for i in range(len(array)):
+		for j in range(len(array) - i - 1):
+			if array[j] > array[j+1]:
+				temp = array[j]
+				array[j] = array[j+1]
+				array[j+1] = temp
 
-insertionSort([4, 3, 2, 10, 12, 1, 5, 6])
+    return array
+
+assert bubbleSort([3,4]) == [3,4]
+assert bubbleSort([4, 3]) == [3,4]
+assert bubbleSort([4, 3, 2]) == [2,3,4]
+assert bubbleSort([4, 3, 2, 10]) == [2,3,4,10]
+assert bubbleSort([4, 3, 2, 10, 12, -1, 5, 6]) == [-1,2,3,4,5,6,10,12]
+assert bubbleSort([4, 3, 2, 10, 12, -1, 5, -6]) == [-6,-1,2,3,4,5,10,12]
