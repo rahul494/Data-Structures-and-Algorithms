29c20af593303892f01aef7fb6479f144c50fa36
Rahul Sharma <sharmarah494@gmail.com>
2020-06-29 10:04:46 -0400

    Update balancedStringSplit.py

--- Strings/balancedStringSplit.py
+++ Strings/balancedStringSplit.py
@@ -1,4 +1,4 @@
-# 1221. Split a String in Balanced Strings
+# Leetcode 1221. Split a String in Balanced Strings
 
 # Balanced strings are those who have equal quantity of 'L' and 'R' characters.
 # Given a balanced string s split it in the maximum amount of balanced strings.
