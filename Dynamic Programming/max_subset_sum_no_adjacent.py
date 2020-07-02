24d857d791cadce16b770946b139ff6ac9f8ed16
rahul494 <rahul494@my.yorku.ca>
2020-07-02 11:37:10 -0400

    Added comments + test cases

--- Dynamic Programming/max_subset_sum_no_adjacent.py
+++ Dynamic Programming/max_subset_sum_no_adjacent.py
@@ -1,7 +1,21 @@
+ # AlgoExpert - Max Subset Sum No Adjacent
+ 
+#   Write a function that takes in an array of positive integers and returns the
+#   maximum sum of non-adjacent elements in the array.
+#   If a sum can't be generated, the function should return 0
+
+#   Sample Input
+
+#     array = [75, 105, 120, 75, 90, 135]
+
+#   Output
+
+#     330 (75 + 120 + 135) 
+
 def maxSubsetSumNoAdjacent(array):
 
     if not len(array):
-        return
+        return 0
     elif len(array) == 1:
         return array[0]
 
@@ -15,4 +29,10 @@ def maxSubsetSumNoAdjacent(array):
 
     return first
 
-print(maxSubsetSumNoAdjacent([7, 10, 12, 7, 9, 14]))
+assert maxSubsetSumNoAdjacent([7, 10, 12, 7, 9, 14]) == 33
+assert maxSubsetSumNoAdjacent([75, 105, 120, 75, 90, 135]) == 330
+assert maxSubsetSumNoAdjacent([]) == 0
+assert maxSubsetSumNoAdjacent([1]) == 1
+assert maxSubsetSumNoAdjacent([1, 2]) == 2
+
+print('All tests have passed successfully')
