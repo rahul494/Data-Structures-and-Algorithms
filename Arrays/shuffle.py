a4d056e854a052478e3ca02bc46aab0413131f16
rahul494 <rahul494@my.yorku.ca>
2020-07-03 22:27:05 -0400

    Added test cases

--- Arrays/shuffle.py
+++ Arrays/shuffle.py
@@ -5,22 +5,29 @@
 # Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 
 # Input: nums = [2,5,1,3,4,7], n = 3
-# Output: [2,3,5,4,1,7] 
+# Output: [2,3,5,4,1,7]
 
 # Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
 
-class Solution(object):
-    def shuffle(self, nums, n):
-        """
-        :type nums: List[int]
-        :type n: int
-        :rtype: List[int]
-        """
-        ans = []
-        
-        for i in range(n):
-            ans.append(nums[i])
-            ans.append(nums[i + n])
-        
-        return ans
-        
+
+def shuffle(nums, n):
+    """
+    :type nums: List[int]
+    :type n: int
+    :rtype: List[int]
+    """
+    ans = []
+
+    for i in range(n):
+        ans.append(nums[i])
+        ans.append(nums[i + n])
+
+    return ans
+
+
+assert shuffle([2, 5, 1, 3, 4, 7], 3) == [2, 3, 5, 4, 1, 7]
+assert shuffle([2, 3, 4, 7], 2) == [2, 4, 3, 7]
+assert shuffle([1, 2], 1) == [1, 2]
+assert shuffle([], 0) == []
+
+print('All tests have passed sucessfully')
