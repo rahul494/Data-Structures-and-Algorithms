a66fee4d06676093f8b9e5d7222a8df47d6fe449
rahul494 <rahul494@my.yorku.ca>
2020-07-08 20:48:18 -0400

    Added DP Fibonacci solution + test cases

--- Dynamic Programming/fibonacci_dp.py
+++ Dynamic Programming/fibonacci_dp.py
@@ -5,7 +5,7 @@
 # and (n - 2)th numbers. Write a function that takes in an integer n  and returns the nth Fibonacci number.
 
 # Important note: the Fibonacci sequence is often defined with its first two
-# numbers as F0 = 0 and F1 = 1. . For the purpose of this question, the first Fibonacci number is 
+# numbers as F0 = 0 and F1 = 1. . For the purpose of this question, the first Fibonacci number is
 # F0; therefore, getNthFib(1) is equal to F0, getNthFib(2) is equal to F1, etc...
 
 # Sample Input #1
@@ -21,18 +21,29 @@
 #   5
 
 def getNthFib(n):
-    if(n == 1):
-        return 0
-    
-    elif(n == 2):
-        return 1
+    memo = [0, 1]
 
+    while len(memo) < n + 1:
+        memo.append(0)
+
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
     else:
-        return getNthFib(n-1) + getNthFib(n-2)
+        if memo[n - 1] == 0:
+            memo[n - 1] = getNthFib(n - 1)
+        if memo[n - 2] == 0:
+            memo[n - 2] = getNthFib(n - 2)
+
+        memo[n] = getNthFib(n - 1) + getNthFib(n - 2)
+        return memo[n]
+
 
 assert getNthFib(1) == 0
 assert getNthFib(2) == 1
 assert getNthFib(5) == 3
 assert getNthFib(6) == 5
+assert getNthFib(7) == 8
 
 print('All tests have passed sucessfully')
