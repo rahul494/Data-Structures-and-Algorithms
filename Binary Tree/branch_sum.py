2418cfaea74e00db13d72e7b959753dcc556934c
rahul494 <rahul494@my.yorku.ca>
2020-10-29 17:28:09 -0400

    Defined binary tree

---
+++ Binary Tree/branch_sum.py
@@ -0,0 +1,17 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def branchSums(root):
+    # Write your code here.
+    pass
