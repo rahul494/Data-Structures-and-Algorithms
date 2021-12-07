156038f1812a891d80bae4d01557eb2a70b1d95c
rahul494 <sharmarah494@gmail.com>
2021-12-06 22:27:17 -0500

    Added O(n) solution

--- Arrays/find_averages_of_subarrays.py
+++ Arrays/find_averages_of_subarrays.py
@@ -4,7 +4,7 @@ def find_averages_of_subarrays_naive(k, arr):
         sum = 0.0
         for j in range(i, i+k):
             sum += arr[j]
-        result.append(sum/5)
+        result.append(sum/k)
 
     return result
 
@@ -13,17 +13,21 @@ def find_averages_of_subarrays(k, arr):
     result = []
     window_sum = 0.0
     window_start = 0
-    for i in range(len(arr) - k + 1):
-        sum = 0.0
-        for j in range(i, i+k):
-            sum += arr[j]
-        result.append(sum/5)
+    for windowEnd in range(len(arr)):
+        window_sum += arr[windowEnd]
+
+        if windowEnd >= k - 1:
+            result.append(window_sum/k)
+            window_sum -= arr[window_start]
+            window_start += 1
 
     return result
 
 
 def main():
     result = find_averages_of_subarrays_naive(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
+    print("Averages of subarrays of size K: " + str(result))
+    result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
     print("Averages of subarrays of size K: " + str(result))
 
 
