92c6efeef603fb9875cdf2633a421c6cb5d37fc9
rahul494 <sharmarah494@gmail.com>
2021-12-07 20:46:44 -0500

    Completed solution to max_sub_array function

--- Arrays/max_sub_array_of_size_k.py
+++ Arrays/max_sub_array_of_size_k.py
@@ -1,7 +1,17 @@
 def max_sub_array_of_size_k(k, arr):
+    max_sum, curr_sum = 0, 0
+    window_start = 0
+    for window_end in range(len(arr)):
+        curr_sum += arr[window_end]
+        if window_end >= k - 1:
+            if max_sum < curr_sum:
+                max_sum = curr_sum
+            curr_sum -= arr[window_start]
+            window_start += 1
 
-    return -1
+    return max_sum
 
 
 # ans: 9
-print("The maximum sub array of size k is " + str(max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])))
+print("The maximum sub array of size k is " +
+      str(max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])))
