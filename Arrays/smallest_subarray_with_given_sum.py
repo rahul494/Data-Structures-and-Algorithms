a578971de827041b7e840cd478b428e4e9411754
rahul494 <sharmarah494@gmail.com>
2022-02-10 17:13:16 -0500

    Added solution

--- Arrays/smallest_subarray_with_given_sum.py
+++ Arrays/smallest_subarray_with_given_sum.py
@@ -1,3 +1,21 @@
+# E.g. [2, 1, 5, 2, 3, 2], target 7
+
+import math
+
+
 def smallest_subarray_with_given_sum(s, arr):
-    # TODO: Write your code here
-    return -1
+    window_sum, window_start = 0
+    min_length = math.inf
+
+    for window_end in range(0, len(arr)):
+        window_sum += arr[window_end]
+
+        while window_sum >= s:
+            min_length = min(min_length, window_end - window_start + 1)
+            window_sum -= arr[window_start]
+            window_start += 1
+
+    if min_length == math.inf:
+        return 0
+
+    return min_length
