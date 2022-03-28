8a446ac349156c86713dab43953ce0474f56c957
rahul494 <sharmarah494@gmail.com>
2022-03-27 21:01:52 -0400

    Added Solution

---
+++ Linked Lists/reverse_linked_list.py
@@ -0,0 +1,10 @@
+def reverse_iterative(self):
+    prev = None
+    cur = self.head
+    while cur:
+        nxt = cur.next
+        cur.next = prev
+        prev = cur
+        cur = nxt
+    
+    self.head = prev
