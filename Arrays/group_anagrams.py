d4b8e89db2208940f4e484e5bf27edf31c885ec5
rahul494 <sharmarah494@gmail.com>
2022-04-04 10:57:34 -0400

    Added solution

---
+++ Arrays/group_anagrams.py
@@ -0,0 +1,17 @@
+def group_anagrams(strings):
+    dict = {}
+
+    for str in strings:
+        key = [0] * 26
+
+        for letter in str:
+            key[ord(letter) - ord('a')] += 1
+
+        if tuple(key) in dict:
+            dict[tuple(key)].append(str)
+        else:
+            dict[tuple(key)] += [str]
+
+    return dict
+
+print(group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
