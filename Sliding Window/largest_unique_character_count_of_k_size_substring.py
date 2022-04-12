f44950522932ad432069d628836310e6dbc6aeb3
rahul494 <sharmarah494@gmail.com>
2022-04-12 10:11:14 -0400

    Added Solution

---
+++ Sliding Window/largest_unique_character_count_of_k_size_substring.py
@@ -0,0 +1,32 @@
+def largest_unique_character_count_of_k_size_subtring(str, k):
+    history = {}
+    largest_unique_count = 0
+    
+    # maintains the indexes of the window boundries
+    low = high = 0
+    
+    while high < len(str):
+
+        if str[high] not in history:
+            history[str[high]] = 1
+            largest_unique_count += 1
+        else:
+            history[str[high]] += 1
+
+        # if window size is greater than k, remove characters of the left
+        if high - low + 1 > k:
+            if history[str[low]] > 2:
+                history[str[low]] -= 1
+            elif history[str[low]] == 2:
+                history[str[low]] -= 1
+            else:
+                del history[str[low]]
+                largest_unique_count -= 1
+
+            low += 1
+
+        high += 1
+
+    return largest_unique_count
+
+print(largest_unique_character_count_of_k_size_subtring("aassssddff", 2))
---
+++ Sliding Window/longest_substring_k _distinct_characters.py
@@ -0,0 +1,8 @@
+def longest_substring_k_distinct_characters(str, k):
+    curr_k = 1
+    
+    pass
+
+print(longest_substring_k_distinct_characters('abcbdbdbbdcdabd', 2)) # bdbdbbd
+print(longest_substring_k_distinct_characters('abcbdbdbbdcdabd', 3)) # bcbdbdbbdcd
+print(longest_substring_k_distinct_characters('abcbdbdbbdcdabd', 5)) # abcbdbdbbdcdabd
