75e42c7e6f80bf70a58ba8333c414fd393901932
rahul494 <sharmarah494@gmail.com>
2022-04-16 17:15:16 -0400

    Added solution

---
+++ Linked Lists/reverse_linked_list.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def add_first(self, node):
+        if self.head:
+            node.next = self.head
+        self.head = node
+
+    def print(self):
+        curr = self.head
+        while curr:
+            print(curr.value)
+            curr = curr.next
+
+    def reverse_list(self):
+        prev = None
+        while self.head:
+            temp = self.head
+            self.head = self.head.next
+
+
+
+
+ll = LinkedList(Node(5))
+ll.add_first(Node(4))
+ll.add_first(Node(3))
+ll.add_first(Node(2))
+ll.add_first(Node(1))
+ll.print()
