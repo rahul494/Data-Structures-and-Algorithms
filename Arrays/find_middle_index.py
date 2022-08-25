798531736bb8298a75f45104b51d8c8f00aefe01
rahul494 <sharmarah494@gmail.com>
2022-08-25 10:00:36 -0400

    Initialized findMiddleIndex solution

---
+++ Arrays/find_middle_index.py
@@ -0,0 +1,10 @@
+def findMiddleIndex(nums):
+    pass
+
+def main():
+    print(findMiddleIndex([2, 3, -1, 8, 4]) == 3)
+    print(findMiddleIndex([1, -1, 4]) == 2)
+    print(findMiddleIndex([2, 5]) == -1)
+    
+if __name__ == '__main__':
+    main()
